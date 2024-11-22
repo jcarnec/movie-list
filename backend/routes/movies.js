10333fac1703ca3f49777003ebb8205cfd70327c
jcarnec <carnecj@tcd.ie>
2024-11-22 14:13:26 +0000

    - fixed relocation

--- backend/routes/movies.js
+++
@@ -1,8 +0,0 @@
-// routes/movies.js
-const express = require('express');
-const router = express.Router();
-const { getMovies } = require('../controllers/moviesController');
-
-router.post('/', getMovies);
-
-module.exports = router;
--- frontend/src/loadMoviesAsyncStore.js
+++
@@ -1,5 +0,0 @@
-import createAsyncStore from './asyncStore.js'
-
-const asyncStore = createAsyncStore();
-
-export default asyncStore;
